15facd1aaa015ff437b9a0ae304e155fd4429840
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-07 11:33:42 -0400

    feat(exercises): implement reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  const stringArray = string.split("");
+  const reversedArray = [];
 
+  for (let index = stringArray.length - 1; index >= 0; index--) {
+    reversedArray.push(stringArray[index]);
+  }
+
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
