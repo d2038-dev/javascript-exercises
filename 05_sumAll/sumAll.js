15187b95131f4f695a6e891c2d2f18575c8844f4
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-09 09:41:23 -0400

    feat(exercises): implement sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = 0;
 
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  if (start < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+
+  for (let index = start; index <= end; index++) {
+    sum += index;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
