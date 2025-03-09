1509e2848dc3efa1da7b64151977f3f053db9968
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-09 10:09:41 -0400

    feat(exercises): implement tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  let tempInCelsius = +(((tempInFahrenheit - 32) * 5) / 9).toFixed(1);
+
+  return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  let tempInFahrenheit = +((tempInCelsius * 9) / 5 + 32).toFixed(1);
+
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
