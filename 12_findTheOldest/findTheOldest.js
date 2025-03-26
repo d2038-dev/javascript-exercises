385ab957924df179e21f95743c5b31a70cad2685
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-26 19:24:39 -0400

    feat(exercises): implement findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const currentYear = new Date().getFullYear();
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const currentAge =
+      (current.yearOfDeath || currentYear) - current.yearOfBirth;
+    if (oldestAge >= currentAge) {
+      return oldest;
+    } else {
+      return current;
+    }
+  });
 };
 
 // Do not edit below this line
