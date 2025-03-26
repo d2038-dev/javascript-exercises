10cc48fec99331af8365a3f883f77aff14dd6dcb
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-26 18:47:37 -0400

    feat(exercises): implement palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const cleanedWord = word
+    .toLowerCase()
+    .split("")
+    .filter((char) => char.match(/[A-Za-z0-9]/))
+    .join("");
 
+  const reversedWord = cleanedWord.split("").reverse().join("");
+
+  return cleanedWord == reversedWord;
 };
 
 // Do not edit below this line
